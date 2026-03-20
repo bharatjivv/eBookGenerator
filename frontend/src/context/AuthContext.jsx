00bf91b99a6c7237b2cebd458e3e8aa5273222eb
BharatJivvTate <hibharatj@gmail.com>
2026-03-20 20:48:10 +0530

    [update] - frontend context and utils built

--- frontend/src/context/AuthContext.jsx
+++ frontend/src/context/AuthContext.jsx
@@ -1,9 +1,40 @@
-import React from 'react'
+import React, { createContext, useContext, useState, useEffect } from 'react';
 
-const AuthContext = () => {
-  return (
-    <div>AuthContext</div>
-  )
+const AuthContext = createContext();
+
+export const useAuth = () => {
+  const context = useContext(AuthContext);
+  if(!context) {
+    throw new Error("useAuth must be used within an AuthProvider")
+  }
+  return context;
 }
 
-export default AuthContext
+export const AuthProvider = ({children}) => {
+  const [user, setUser] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+
+  useEffect(() => {
+    checkAuthStatus();
+  }, []);
+
+  const checkAuthStatus = async () => {};
+
+  const login = (userData, token) => {};
+  const logout = () => {};
+  const updateUser = (updatedUserData) => {};
+  
+  const value = {
+    user, 
+    loading,
+    isAuthenticated,
+    login,
+    logout,
+    updateUser,
+    checkAuthStatus,
+  };
+
+  return <AuthContext.Provider value={value}>{children}</AuthContext.Provider>
+  
+};
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,11 +3,16 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
 import { BrowserRouter } from "react-router-dom";
+import { Toaster } from 'react-hot-toast';
+import { AuthProvider } from "./context/AuthContext.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
+    <AuthProvider>
     <BrowserRouter>
       <App />
+      <Toaster position="botton-right" />
     </BrowserRouter>
+    </AuthProvider>
   </StrictMode>,
 );
